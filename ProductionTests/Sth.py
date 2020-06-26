b4c7fea7def6ed9c6c7488a4dc51f34fa4072230
René Schwaiger <sanssecours@me.com>
2020-06-26 13:25:06 +0200

    STH Test: Remove Excel functionality

--- ProductionTests/Sth.py
+++ ProductionTests/Sth.py
@@ -9,19 +9,16 @@ sys.path.append(sDirName)
 import CanFd
 import time
 import array
-import openpyxl
 import math
 import struct
 from datetime import date
 from shutil import copyfile
-from openpyxl.styles import Font
 from MyToolItNetworkNumbers import MyToolItNetworkNr
 from MyToolItSth import TestConfig, SthErrorWord, fVoltageBattery
 from MyToolItStu import StuErrorWord
 from SthLimits import SthLimits
 from StuLimits import StuLimits
 from MyToolItCommands import *
-from openpyxl.descriptors.base import DateTime
 from os.path import abspath, isfile, join
 from re import escape, search
 from subprocess import run
@@ -38,18 +35,6 @@ iSensorAxis = 1
 uAdc2Acc = 200
 iRssiMin = -75
 bStuPcbOnly = True
-"""
-Get serial number that is stored in the excel file
-"""
-
-
-def sSerialNumber(sExcelFileName):
-    tWorkbook = openpyxl.load_workbook(sExcelFileName)
-    tWorkSheet = tWorkbook["Product Data@0x4"]
-    value = tWorkSheet['E12'].value
-    sSerial = str(value)
-    return sSerial
-
 
 bSkip = False
 sHolderNameInput = None
@@ -92,8 +77,6 @@ class TestSth(unittest.TestCase):
                 self.tSthLimits.uSamplingRateOverSamplesReset,
                 FreshLog=True)
             self.Can.Logger.Info("TestCase: " + str(self._testMethodName))
-            self.sSerialNumber = sSerialNumber(
-                self.sExcelEepromContentFileName)
             self.Can.CanTimeStampStart(
                 self._resetStu()["CanTime"])  # This will also reset the STH
 
@@ -130,9 +113,6 @@ class TestSth(unittest.TestCase):
                     self.sBatchNumber = str(iBatchNr)
                     batchFile.write(self.sBatchNumber)
                     batchFile.close()
-                self.sExcelEepromContentReadBackFileName = sLogName + "_" + self.sSerialNumber + "_" + self.sSthAddr.replace(
-                    ":", "#") + "_ReadBack.xlsx"
-                self.tWorkbookOpenCreate()
                 self._statusWords()
                 self._iSthAdcTemp()
                 self._SthWDog()
@@ -256,266 +236,6 @@ class TestSth(unittest.TestCase):
                                      MyToolItStatData["Wdog"])[:4])
         self.Can.Logger.Info("WatchDog Counter: " + str(WdogCounter))
         return WdogCounter
-
-    """
-    Try to open Excel Sheet. If not able to open, then create a new one
-    """
-
-    def tWorkbookOpenCreate(self):
-        try:
-            self.tWorkbook = openpyxl.load_workbook(self.sTestReport + ".xlsx")
-        except:
-            self.tWorkbook = openpyxl.Workbook()
-            self.tWorkbook.remove_sheet(
-                self.tWorkbook.get_sheet_by_name('Sheet'))
-            self.tWorkSheet = self.tWorkbook.create_sheet(
-                self.sSthAddr.replace(":", "#"))
-            tFont = Font(bold=True, size=20)
-            self.tWorkSheet['A1'] = 'Test Batch Number'
-            self.tWorkSheet['A1'].font = tFont
-            self.tWorkSheet['B1'] = 'Test Name'
-            self.tWorkSheet['B1'].font = tFont
-            self.tWorkSheet['C1'] = 'DateTime'
-            self.tWorkSheet['C1'].font = tFont
-            self.tWorkSheet['D1'] = 'Description'
-            self.tWorkSheet['D1'].font = tFont
-            for i in range(0, 16):
-                self.tWorkSheet[chr(0x45 + i) +
-                                "1"].value = 'Test Case Report ' + str(i)
-                self.tWorkSheet[chr(0x45 + i) + "1"].font = tFont
-            self.tWorkbook.save(self.sTestReport + ".xlsx")
-            self.tWorkbook = openpyxl.load_workbook(self.sTestReport + ".xlsx")
-        self.tWorkSheet = self.tWorkbook.get_sheet_by_name(
-            self.sSthAddr.replace(":", "#"))
-        self.iTestRow = 1
-        while (None != self.tWorkSheet['A' + str(self.iTestRow + 1)].value):
-            self.iTestRow += 1
-        self.tWorkSheetWrite("A", self.iTestRow)
-        self.tWorkSheetWrite("B", self._testMethodName)
-        self.tWorkSheetWrite("C", self.sDateClock)
-
-    """
-    Write Column in Row by value (Note that sColl is e.g. "D")
-    """
-
-    def tWorkSheetWrite(self, sCol, value):
-        tFont = Font(bold=False, size=12)
-        self.tWorkSheet[sCol + str(self.iTestRow + 1)].value = str(value)
-        self.tWorkSheet[sCol + str(self.iTestRow + 1)].font = tFont
-
-    """
-    Transform excel sheet data to byte arrays such that the firmware can write
-    it. Please take always care of byte endian style i.e. MSB vs LSB.
-    """
-
-    def au8excelValueToByteArray(self, worksheet, iIndex):
-        iLength = int(worksheet['C' + str(iIndex)].value)
-        value = worksheet['E' + str(iIndex)].value
-        byteArray = [0] * iLength
-        if None != value:
-            if "UTF8" == worksheet['G' + str(iIndex)].value:
-                try:
-                    value = str(value).encode('utf-8')
-                except Exception as e:
-                    self.Can.Logger.Info(str(e))
-                    value = [0] * iLength
-                iCopyLength = len(value)
-                if iLength < iCopyLength:
-                    iCopyLength = iLength
-                for i in range(0, iCopyLength):
-                    byteArray[i] = value[i]
-            elif "ASCII" == worksheet['G' + str(iIndex)].value:
-                try:
-                    value = str(value).encode('ascii')
-                except Exception as e:
-                    self.Can.Logger.Info(str(e))
-                    value = [0] * iLength
-                iCopyLength = len(value)
-                if iLength < iCopyLength:
-                    iCopyLength = iLength
-                for i in range(0, iCopyLength):
-                    byteArray[i] = value[i]
-            elif "unsigned" == worksheet['G' + str(iIndex)].value:
-                byteArray = au8Value2Array(int(value), iLength)
-            elif "float" == worksheet['G' + str(iIndex)].value:
-                value = float(value)
-                value = struct.pack('f', value)
-                value = int.from_bytes(value, byteorder='little')
-                byteArray = au8Value2Array(int(value), 4)
-            else:
-                if "0" == value or 0 == value:
-                    value = "[0x0]"
-                value = value[1:-1].split(',')
-                for i in range(0, len(value)):
-                    byteArray[i] = int(value[i], 16)
-        return byteArray
-
-    """
-    How many bytes are represented in Excel Sheet
-    """
-
-    def iExcelSheetPageLength(self, worksheet):
-        totalLength = 0
-        for i in range(2, 256 + 2):
-            if None != worksheet['A' + str(i)].value:
-                length = int(worksheet['C' + str(i)].value)
-                totalLength += length
-            else:
-                break
-        return totalLength
-
-    """
-    Read Excel Sheet and write it to STH
-    """
-
-    def sExcelSheetWrite(self, namePage, iReceiver):
-        sError = None
-        workbook = openpyxl.load_workbook(self.sExcelEepromContentFileName)
-        if workbook:
-            for worksheetName in workbook.sheetnames:
-                name = str(worksheetName).split('@')
-                address = int(name[1], base=16)
-                name = name[0]
-                if namePage == name:
-                    worksheet = workbook.get_sheet_by_name(worksheetName)
-                    # Prepare Write Data
-                    au8WriteData = [0] * 256
-                    iByteIndex = 0
-                    for i in range(2, 256 + 2, 1):
-                        if None != worksheet['A' + str(i)].value:
-                            au8ElementData = self.au8excelValueToByteArray(
-                                worksheet, i)
-                            for j in range(0, len(au8ElementData), 1):
-                                au8WriteData[iByteIndex +
-                                             j] = au8ElementData[j]
-                            iLength = int(worksheet['C' + str(i)].value)
-                            iByteIndex += iLength
-                        else:
-                            break
-                    iWriteLength = self.iExcelSheetPageLength(worksheet)
-                    if 0 != iWriteLength % 4:
-                        iWriteLength += 4
-                        iWriteLength -= (iWriteLength % 4)
-                    au8WriteData = au8WriteData[0:iWriteLength]
-                    self.Can.Logger.Info("Write Content: " +
-                                         payload2Hex(au8WriteData))
-                    for offset in range(0, iWriteLength, 4):
-                        au8WritePackage = au8WriteData[offset:offset + 4]
-                        au8Payload = [address, 0xFF & offset, 4, 0]
-                        au8Payload.extend(au8WritePackage)
-                        self.Can.cmdSend(iReceiver,
-                                         MyToolItBlock["Eeprom"],
-                                         MyToolItEeprom["Write"],
-                                         au8Payload,
-                                         log=False)
-            try:
-                workbook.close()
-            except Exception as e:
-                sError = "Could not close file: " + str(e)
-                self.Can.Logger.Info(sError)
-        return sError
-
-    """
-    Try to get of an illegal UTF8 character
-    """
-
-    def vUnicodeIllegalRemove(self, value, character):
-        while (True):
-            try:
-                value.remove(character)
-            except:
-                break
-        return value
-
-    """
-    Write Byte Array into Excel Sheet with care of the format
-    """
-
-    def iExcelSheetPageValue(self, worksheet, aBytes):
-        i = 2
-        iTotalLength = 0
-        while len(aBytes) > iTotalLength:
-            if None != worksheet['A' + str(i)].value:
-                iLength = worksheet['C' + str(i)].value
-                value = aBytes[iTotalLength:iTotalLength + iLength]
-                if "UTF8" == worksheet['G' + str(i)].value:
-                    try:
-                        value = self.vUnicodeIllegalRemove(value, 0)
-                        value = self.vUnicodeIllegalRemove(value, 255)
-                        value = array.array('b', value).tostring().decode(
-                            'utf-8', 'replace')
-                    except Exception as e:
-                        self.Can.Logger.Info(str(e))
-                        value = ""
-                elif "ASCII" == worksheet['G' + str(i)].value:
-                    value = sArray2String(value)
-                elif "unsigned" == worksheet['G' + str(i)].value:
-                    value = str(iMessage2Value(value))
-                elif "float" == worksheet['G' + str(i)].value:
-                    if None != value:
-                        pass
-                        # value = au8ChangeEndianOrder(value)
-                    else:
-                        value = 0.0
-                    self.Can.Logger.Info("Value from EEPROM: " + str(value))
-                    value = bytearray(value)
-                    value = struct.unpack('f', value)[0]
-                    value = str(value)
-                    self.Can.Logger.Info("Value as float: " + str(value))
-                else:
-                    value = payload2Hex(value)
-                value = str(value)
-                try:
-                    worksheet['E' + str(i)] = str(value).encode('utf8')
-                except:
-                    pass
-                iTotalLength += iLength
-                i += 1
-            else:
-                break
-        return iTotalLength
-
-    """
-    Read EEPROM page to write values in Excel Sheet
-    """
-
-    def sExcelSheetRead(self, namePage, iReceiver):
-        sError = None
-        workbook = openpyxl.load_workbook(
-            self.sExcelEepromContentReadBackFileName)
-        if workbook:
-            for worksheetName in workbook.sheetnames:
-                name = str(worksheetName).split('@')
-                address = int(name[1], base=16)
-                name = name[0]
-                if namePage == name:
-                    worksheet = workbook.get_sheet_by_name(worksheetName)
-                    pageContent = []
-                    readLength = self.iExcelSheetPageLength(worksheet)
-                    readLengthAlligned = readLength
-                    if 0 != readLengthAlligned % 4:
-                        readLengthAlligned += 4
-                        readLengthAlligned -= (readLengthAlligned % 4)
-                    for offset in range(0, readLengthAlligned, 4):
-                        payload = [address, 0xFF & offset, 4, 0, 0, 0, 0, 0]
-                        index = self.Can.cmdSend(iReceiver,
-                                                 MyToolItBlock["Eeprom"],
-                                                 MyToolItEeprom["Read"],
-                                                 payload,
-                                                 log=False)
-                        readBackFrame = self.Can.getReadMessageData(index)[4:]
-                        pageContent.extend(readBackFrame)
-                    pageContent = pageContent[0:readLength]
-                    self.Can.Logger.Info("Read Data: " +
-                                         payload2Hex(pageContent))
-                    self.iExcelSheetPageValue(worksheet, pageContent)
-            try:
-                workbook.save(self.sExcelEepromContentReadBackFileName)
-            except Exception as e:
-                sError = "Could not save file(Opened by another application?): " + str(
-                    e)
-                self.Can.Logger.Info(sError)
-        return sError
 
     def test0000FirmwareFlash(self):
         """Upload bootloader into STH"""
