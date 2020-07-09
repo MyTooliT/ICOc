03fcf626f0a903dd52a28b028cc1e906686e9a7e
René Schwaiger <sanssecours@me.com>
2020-07-09 13:39:25 +0200

    Report: Add initial version of report class

---
+++ mytoolit/report.py
@@ -0,0 +1,23 @@
+# -- Imports ------------------------------------------------------------------
+
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfgen.canvas import Canvas
+
+# -- Class --------------------------------------------------------------------
+
+
+class Report:
+    """Generate test reports using ReportLab"""
+
+    def __init__(self):
+        """Initialize the report"""
+
+        self.canvas = Canvas('report.pdf', bottomup=0, pagesize=A4)
+        self.width, self.height = A4
+        self.canvas.drawString(100, 100, "MyTooliT Report")
+
+    def __exit__(self):
+        """Store the PDF report"""
+
+        self.canvas.save()
+        self.canvas.showPage()
