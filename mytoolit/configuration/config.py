67f2cee7fff84f0116fc0a7ef44eb1728e2ed3c0
René Schwaiger <sanssecours@me.com>
2020-07-13 16:31:33 +0200

    Config: Use relative location for file path

--- mytoolit/configuration/config.py
+++ mytoolit/configuration/config.py
@@ -1,6 +1,11 @@
 from dynaconf import Dynaconf
+from os.path import abspath, dirname, join
+
+repository_root = dirname(dirname(dirname(abspath(__file__))))
 
 settings = Dynaconf(
     envvar_prefix="DYNACONF",
-    settings_file=['Configuration/config.yaml'],
+    settings_file=[
+        join(join(repository_root, 'Configuration'), 'config.yaml')
+    ],
 )
