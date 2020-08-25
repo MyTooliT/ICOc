cc58d983cc43555bd974370868f9a891df0a0500
René Schwaiger <sanssecours@me.com>
2020-08-25 15:43:30 +0200

    CAN: Improve readability of expressions

--- CanFd.py
+++ CanFd.py
@@ -223,11 +223,11 @@ class CanFd(object):
 
     def WriteFrame(self, CanMsg):
         returnMessage = CanMsg
-        if "Error" != returnMessage:
+        if returnMessage != "Error":
             self.tCanReadWriteMutex.acquire()
             returnMessage = self.pcan.Write(self.m_PcanHandle, CanMsg)
             self.tCanReadWriteMutex.release()
-            if (PCAN_ERROR_OK != returnMessage):
+            if returnMessage != PCAN_ERROR_OK:
                 print("WriteFrame bError: " + hex(returnMessage))
                 self.Logger.Info("WriteFrame bError: " + hex(returnMessage))
                 returnMessage = "Error"
