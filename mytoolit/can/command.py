8ea12d23605bb46059c3722324316fcdd9aac974
René Schwaiger <sanssecours@me.com>
2020-09-01 11:54:28 +0200

    Command: Create initial version of class

---
+++ mytoolit/can/command.py
@@ -0,0 +1,78 @@
+# -- Class --------------------------------------------------------------------
+
+
+class Command:
+    """This class represents a command including error and acknowledge bits"""
+
+    def __init__(
+            self,
+            *command,
+            block=None,
+            block_command=None,
+            request=True,
+            error=None,
+    ):
+        """Create a new command from the given arguments
+
+        Usually you will either specify the command directly, or provide
+        block, block_command and values for the error and acknowledge/request
+        bits. If you decide to specify both the command value and one of the
+        keyword arguments, then the keyword arguments will be used to
+        overwrite specific parts of the command. For more information, please
+        take a look at the examples.
+
+
+        Arguments
+        ---------
+
+        command:
+            A 16 bit number that specifies the command including acknowledge
+            and error bits (16 bit)
+
+        block:
+            A 6 bit number that specifies the block of the command
+
+        block_command:
+            A 8 bit number that stores the number of the command in the
+            specified block
+
+        request:
+            A boolean value that specifies if the command is for a request or
+            an acknowledgement
+
+        error:
+            A boolean value that defines if there was an error or not
+
+        Examples
+        --------
+
+        >>> command = Command(block=0, block_command=0)
+
+        """
+
+        def set_part(start, width, number):
+            """Store bit pattern number at bit start of the identifier"""
+
+            command_ones = 0xffff
+            mask = 2**width - 1
+
+            # Set all bits for targeted part to 0
+            self.value &= (mask << start) ^ command_ones
+            # Make sure we use the correct number of bits for number
+            number = number & mask
+            # Set command bits to given value
+            self.value |= number << start
+
+        self.value = command[0] if command else 0
+
+        if block:
+            set_part(start=10, width=6, number=block)
+
+        if block_command:
+            set_part(start=2, width=8, number=block_command)
+
+        if request:
+            set_part(start=1, width=1, number=int(bool(request)))
+
+        if error:
+            set_part(start=0, width=1, number=int(not (error)))
