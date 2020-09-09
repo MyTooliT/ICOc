9bdcf004d4c5dfa771d8466ca348901933e93ce2
René Schwaiger <sanssecours@me.com>
2020-09-09 15:11:51 +0200

    Status: Add support for status word 0 of STU

--- mytoolit/can/status.py
+++ mytoolit/can/status.py
@@ -1,5 +1,6 @@
 # -- Imports ------------------------------------------------------------------
 
+from enum import Enum
 from os.path import abspath, dirname
 from sys import path as module_path
 
@@ -9,26 +10,37 @@ module_path.append(repository_root)
 
 from MyToolItCommands import NetworkStateName
 
-# -- Class --------------------------------------------------------------------
+# -- Classes ------------------------------------------------------------------
+
+
+class NodeType(Enum):
+    """Specifies the type of a node in the network"""
+    STH = 1
+    STU = 2
 
 
 class StatusWord0:
     """Wrapper class for status word 0"""
 
-    def __init__(self, value):
-        """Initialize the status word with the given value
+    def __init__(self, value, node=NodeType.STH):
+        """Initialize the status using the given arguments
 
-        Argument
-        --------
+        Arguments
+        ---------
 
         value:
             A 32 bit integer or list of bytes that specifies the value of the
             status word
+
+        node:
+            Specifies the network unit the status word belongs to
         """
 
         # Currently only the first byte (of the little endian version) of
         # status word 0 contains (non-reserved) data
         self.value = value if isinstance(value, int) else value[0]
+
+        self.type = node
 
     def __repr__(self):
         """Retrieve the textual representation of the status word
@@ -47,16 +59,36 @@ class StatusWord0:
         >>> StatusWord0([0b1010, 0, 0, 0])
         State: Network State Operating, No Error
 
-        >>> StatusWord0(0b1)
+        >>> StatusWord0(0b1, NodeType.STH)
         State: Network State Failure, Error
+
+        >>> StatusWord0(0b1101010,
+        ...             NodeType.STU) # doctest:+NORMALIZE_WHITESPACE
+        State: Network State Operating, No Error, Radio Port Disabled,
+        CAN Port Enabled, Bluetooth Connected
         """
 
         error = self.value & 1
         state = (self.value >> 1) & 0b111
         state_name = NetworkStateName[state]
 
-        description = ", ".join(
-            [f"State: {state_name}", f"{'' if error else 'No '}Error"])
+        attributes = [f"State: {state_name}", f"{'' if error else 'No '}Error"]
+
+        if self.type == NodeType.STU:
+            radio_port_enabled = self.value >> 4 & 1
+            can_port_enabled = self.value >> 5 & 1
+            bluetooth_connected = self.value >> 6 & 1
+
+            attributes.extend([
+                "Radio Port {}".format(
+                    "Enabled" if radio_port_enabled else "Disabled"),
+                "CAN Port {}".format(
+                    "Enabled" if can_port_enabled else "Disabled"),
+                "Bluetooth {}".format(
+                    "Connected" if bluetooth_connected else "Disconnected"),
+            ])
+
+        description = ", ".join(attributes)
 
         return description
 
