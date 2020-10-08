97456a9b619c2d40fe63824420a9386f4f9973b4
René Schwaiger <sanssecours@me.com>
2020-10-08 14:47:41 +0200

    CAN: Invert code to remove indentation

--- CanFd.py
+++ CanFd.py
@@ -199,16 +199,19 @@ class CanFd(object):
 
     def WriteFrame(self, CanMsg):
         returnMessage = CanMsg
-        if returnMessage != "Error":
-            with self.tCanReadWriteMutex:
-                returnMessage = self.pcan.Write(self.m_PcanHandle, CanMsg)
-            if returnMessage != PCAN_ERROR_OK:
-                print("WriteFrame bError: " + hex(returnMessage))
-                self.Logger.Info("WriteFrame bError: " + hex(returnMessage))
-                returnMessage = "Error"
-                self.__exitError("Driver Problems or Physical Layer Problems")
-            else:
-                getLogger('can').debug(f"{Message(CanMsg)}")
+        if returnMessage == "Error":
+            return returnMessage
+
+        with self.tCanReadWriteMutex:
+            returnMessage = self.pcan.Write(self.m_PcanHandle, CanMsg)
+        if returnMessage != PCAN_ERROR_OK:
+            print("WriteFrame bError: " + hex(returnMessage))
+            self.Logger.Info("WriteFrame bError: " + hex(returnMessage))
+            returnMessage = "Error"
+            self.__exitError("Driver Problems or Physical Layer Problems")
+        else:
+            getLogger('can').debug(f"{Message(CanMsg)}")
+
         return returnMessage
 
     def WriteFrameWaitAckOk(self, message):
