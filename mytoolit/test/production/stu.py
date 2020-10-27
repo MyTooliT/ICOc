e2838e80e67c7a901055fd6d4c750ecbecf84f74
René Schwaiger <sanssecours@me.com>
2020-10-27 14:19:27 +0100

    STU Test: Read MAC address

--- mytoolit/test/production/stu.py
+++ mytoolit/test/production/stu.py
@@ -8,14 +8,51 @@ from unittest import main
 repository_root = dirname(dirname(dirname(dirname(abspath(__file__)))))
 module_path.append(repository_root)
 
-from mytoolit.test.production import TestNode
+from mytoolit.test.production import TestNode, create_attribute
 from mytoolit.unittest import ExtendedTestRunner
+
+from MyToolItNetworkNumbers import MyToolItNetworkNr
+from MyToolItCommands import int_to_mac_address
 
 # -- Class --------------------------------------------------------------------
 
 
 class TestSTU(TestNode):
     """This class contains tests for the Stationary Transceiver Unit (STU)"""
+
+    @classmethod
+    def _collect_node_data(cls):
+        """Collect data about STU
+
+        Returns
+        -------
+
+        An iterable of defined STU attributes stored in simple name space
+        objects
+        """
+
+        possible_attributes = [
+            create_attribute("Bluetooth Address", "{cls.bluetooth_mac}"),
+        ]
+
+        # Check available read hardware attributes
+        attributes = []
+        for attribute in possible_attributes:
+            try:
+                attribute.value = str(attribute.value).format(cls=cls)
+                attributes.append(attribute)
+            except AttributeError:
+                pass
+
+        return attributes
+
+    def _read_data(self):
+        """Read data from connected STU"""
+
+        cls = type(self)
+
+        cls.bluetooth_mac = int_to_mac_address(
+            self.can.BlueToothAddress(MyToolItNetworkNr['STU1']))
 
 
 # -- Main ---------------------------------------------------------------------
