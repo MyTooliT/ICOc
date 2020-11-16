ee551877c29c385f384347672a47f26c35faafdc
René Schwaiger <sanssecours@me.com>
2020-11-16 15:20:30 +0100

    Node: Add string representation

--- mytoolit/can/node.py
+++ mytoolit/can/node.py
@@ -1,3 +1,12 @@
+# -- Imports ------------------------------------------------------------------
+
+from os.path import abspath, dirname
+from sys import path as module_path
+
+# Add path for custom libraries
+repository_root = dirname(dirname(dirname(abspath(__file__))))
+module_path.append(repository_root)
+
 # -- Class --------------------------------------------------------------------
 
 
@@ -24,7 +33,46 @@ class Node:
 
         """
 
-        self.value = node if node else 0
+        self.value = node[0] if node else 0
+
+    def __repr__(self):
+        """Return the string representation of the current node
+
+        Returns
+        -------
+
+        A string that describes the node
+
+        Examples
+        --------
+
+        >>> Node(0)
+        Broadcast With Acknowledgment
+
+        >>> Node(31)
+        Broadcast Without Acknowledgment
+
+        >>> Node(10)
+        STH 10
+
+        >>> Node(15)
+        SPU 1
+
+        >>> Node(18)
+        STU 2
+
+        """
+
+        if self.value == 0 or self.value == 31:
+            return "Broadcast With{} Acknowledgment".format("" if self.value ==
+                                                            0 else "out")
+        if 1 <= self.value <= 14:
+            return f"STH {self.value}"
+
+        if 15 <= self.value <= 16:
+            return f"SPU {self.value-14}"
+
+        return f"STU {self.value-16}"
 
 
 # -- Main ---------------------------------------------------------------------
