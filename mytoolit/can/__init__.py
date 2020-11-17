5d45caad8986dd6cacd98e9dce2de4c3f46b7b9d
René Schwaiger <sanssecours@me.com>
2020-11-17 13:59:15 +0100

    CAN: Export node class

--- mytoolit/can/__init__.py
+++ mytoolit/can/__init__.py
@@ -3,5 +3,6 @@
 from .command import Command
 from .identifier import Identifier
 from .message import Message
+from .node import Node
 from .status import (NodeStatusSTH, NodeStatusSTU, ErrorStatusSTH,
                      ErrorStatusSTU)
