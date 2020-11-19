b9e9b9b7d128875ed5da854e0ce3e674e013ee30
René Schwaiger <sanssecours@me.com>
2020-11-19 10:32:42 +0100

    EEPROM: Add status byte string representation

--- mytoolit/eeprom/eeprom_status.py
+++ mytoolit/eeprom/eeprom_status.py
@@ -15,3 +15,39 @@ class EEPROMStatus:
         """
 
         self.value = value
+
+    def __repr__(self):
+        """Return the string representation of the status byte
+
+        Returns
+        -------
+
+        A string that describes the current value of the status byte
+
+        Examples
+        --------
+
+        >>> EEPROMStatus(0xac)
+        Initialized (0xac)
+
+        >>> EEPROMStatus(0x13)
+        Uninitialized (0x13)
+
+        >>> EEPROMStatus(0xCA)
+        Locked (0xca)
+        """
+
+        value = self.value
+
+        description = "Initialized" if value == 0xac else (
+            "Locked" if value == 0xca else "Uninitialized")
+
+        return f"{description} (0x{value:02x})"
+
+
+# -- Main ---------------------------------------------------------------------
+
+if __name__ == '__main__':
+    from doctest import testmod
+
+    testmod()
