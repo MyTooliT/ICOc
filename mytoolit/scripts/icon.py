650dbba0d31dbf437bdd416be5ef18149d8fc717
René Schwaiger <sanssecours@me.com>
2021-01-19 12:25:57 +0100

    ICOn: Add code to connect & disconnect

--- mytoolit/scripts/icon.py
+++ mytoolit/scripts/icon.py
@@ -1,6 +1,10 @@
 # -- Imports ------------------------------------------------------------------
 
-from mytoolit.old import Network
+from time import sleep
+
+from mytoolit.can import Node
+from mytoolit.config import settings
+from mytoolit.old.network import Network
 
 # -- Main ---------------------------------------------------------------------
 
@@ -8,6 +12,17 @@ from mytoolit.old import Network
 def main():
     network = Network()
 
+    stu1 = Node('STU 1')
+    network.reset_node(stu1)
+
+    network.bBlueToothConnectPollingName(stu1.value,
+                                         settings.sth_name(),
+                                         log=False)
+    sleep(1)  # Wait for connection
+
+    print(f"Connected to “{network.read_eeprom_name()}”")
+
+    network.bBlueToothDisconnect(stu1.value)
     network.__exit__()  # Cleanup resources (read thread)
 
 
