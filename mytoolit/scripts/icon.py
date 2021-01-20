f615f48d23eb12233942ce76cd2cb1d5d3097c3f
René Schwaiger <sanssecours@me.com>
2021-01-20 13:16:37 +0100

    EEPROM Check: Print content of page 1

--- mytoolit/scripts/icon.py
+++ mytoolit/scripts/icon.py
@@ -6,15 +6,13 @@ from mytoolit.can import Node
 from mytoolit.config import settings
 from mytoolit.old.network import Network
 
-# -- Main ---------------------------------------------------------------------
+# -- Functions ----------------------------------------------------------------
 
 
-def main():
+def connect():
     network = Network()
-
     stu1 = Node('STU 1')
     network.reset_node(stu1)
-
     network.bBlueToothConnectPollingName(stu1.value,
                                          settings.sth_name(),
                                          log=False)
@@ -22,8 +20,32 @@ def main():
 
     print(f"Connected to “{network.read_eeprom_name()}”")
 
-    network.bBlueToothDisconnect(stu1.value)
+    return network
+
+
+def disconnect(network):
+
+    network.bBlueToothDisconnect(Node('STU 1').value)
     network.__exit__()  # Cleanup resources (read thread)
+
+
+# -- Main ---------------------------------------------------------------------
+
+
+def main():
+
+    network = connect()
+
+    page1 = network.read_eeprom(address=1, offset=0, length=256)
+    print("\nContent of EEPROM page 1:\n")
+    bytes_per_line = 8
+    for byte in range(0, len(page1), bytes_per_line):
+        print(f"{byte:3}: ", end='')
+        byte_representation = " ".join(["{:3}"] * bytes_per_line).format(
+            *page1[byte:byte + bytes_per_line])
+        print(byte_representation)
+
+    disconnect(network)
 
 
 if __name__ == '__main__':
