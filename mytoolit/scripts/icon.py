2bf3f809d6e90495a8f55330ad85acddb7912cf8
René Schwaiger <sanssecours@me.com>
2021-01-20 16:42:27 +0100

    EEPROM Check: Split code into smaller functions

--- mytoolit/scripts/icon.py
+++ mytoolit/scripts/icon.py
@@ -6,18 +6,29 @@ from mytoolit.old.network import Network
 # -- Functions ----------------------------------------------------------------
 
 
-def connect():
+def connect_network():
     network = Network()
-    stu1 = Node('STU 1')
-    network.reset_node(stu1)
+    network.reset_node('STU 1')
+    return network
 
-    mac_address = "08:6b:d7:01:de:81"
+
+def connect_bluetooth(network, mac_address):
     mac_address_hex = hex(int("".join(mac_address.split(":")), 16))
-    network.bBlueToothConnectPollingAddress(stu1.value, mac_address_hex)
+    network.bBlueToothConnectPollingAddress(
+        Node('STU 1').value, mac_address_hex)
 
     print(f"Connected to “{network.read_eeprom_name()}”")
 
-    return network
+
+def read_eeprom_page(network):
+    page1 = network.read_eeprom(address=1, offset=0, length=256)
+    print("\nContent of EEPROM page 1:\n")
+    bytes_per_line = 8
+    for byte in range(0, len(page1), bytes_per_line):
+        print(f"{byte:3}: ", end='')
+        byte_representation = " ".join(["{:3}"] * bytes_per_line).format(
+            *page1[byte:byte + bytes_per_line])
+        print(byte_representation)
 
 
 def disconnect(network):
@@ -31,17 +42,9 @@ def disconnect(network):
 
 def main():
 
-    network = connect()
-
-    page1 = network.read_eeprom(address=1, offset=0, length=256)
-    print("\nContent of EEPROM page 1:\n")
-    bytes_per_line = 8
-    for byte in range(0, len(page1), bytes_per_line):
-        print(f"{byte:3}: ", end='')
-        byte_representation = " ".join(["{:3}"] * bytes_per_line).format(
-            *page1[byte:byte + bytes_per_line])
-        print(byte_representation)
-
+    network = connect_network()
+    connect_bluetooth(network, "08:6b:d7:01:de:81")
+    read_eeprom_page(network)
     disconnect(network)
 
 
