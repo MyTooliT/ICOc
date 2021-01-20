b6e4436607488b03dbd9d8ae7600d15c1be3ecd2
René Schwaiger <sanssecours@me.com>
2021-01-20 16:46:18 +0100

    EEPROM Check: Move code into class

--- mytoolit/scripts/icon.py
+++ mytoolit/scripts/icon.py
@@ -3,38 +3,35 @@
 from mytoolit.can import Node
 from mytoolit.old.network import Network
 
-# -- Functions ----------------------------------------------------------------
+# -- Class --------------------------------------------------------------------
 
 
-def connect_network():
-    network = Network()
-    network.reset_node('STU 1')
-    return network
+class EEPROM_Check:
 
+    def __init__(self):
+        self.network = Network()
+        self.network.reset_node('STU 1')
 
-def connect_bluetooth(network, mac_address):
-    mac_address_hex = hex(int("".join(mac_address.split(":")), 16))
-    network.bBlueToothConnectPollingAddress(
-        Node('STU 1').value, mac_address_hex)
+    def connect_bluetooth(self, mac_address):
+        mac_address_hex = hex(int("".join(mac_address.split(":")), 16))
+        self.network.bBlueToothConnectPollingAddress(
+            Node('STU 1').value, mac_address_hex)
 
-    print(f"Connected to “{network.read_eeprom_name()}”")
+        print(f"Connected to “{self.network.read_eeprom_name()}”")
 
+    def read_eeprom_page(self):
+        page1 = self.network.read_eeprom(address=1, offset=0, length=256)
+        print("\nContent of EEPROM page 1:\n")
+        bytes_per_line = 8
+        for byte in range(0, len(page1), bytes_per_line):
+            print(f"{byte:3}: ", end='')
+            byte_representation = " ".join(["{:3}"] * bytes_per_line).format(
+                *page1[byte:byte + bytes_per_line])
+            print(byte_representation)
 
-def read_eeprom_page(network):
-    page1 = network.read_eeprom(address=1, offset=0, length=256)
-    print("\nContent of EEPROM page 1:\n")
-    bytes_per_line = 8
-    for byte in range(0, len(page1), bytes_per_line):
-        print(f"{byte:3}: ", end='')
-        byte_representation = " ".join(["{:3}"] * bytes_per_line).format(
-            *page1[byte:byte + bytes_per_line])
-        print(byte_representation)
-
-
-def disconnect(network):
-
-    network.bBlueToothDisconnect(Node('STU 1').value)
-    network.__exit__()  # Cleanup resources (read thread)
+    def disconnect(self):
+        self.network.bBlueToothDisconnect(Node('STU 1').value)
+        self.network.__exit__()  # Cleanup resources (read thread)
 
 
 # -- Main ---------------------------------------------------------------------
@@ -42,10 +39,10 @@ def disconnect(network):
 
 def main():
 
-    network = connect_network()
-    connect_bluetooth(network, "08:6b:d7:01:de:81")
-    read_eeprom_page(network)
-    disconnect(network)
+    check = EEPROM_Check()
+    check.connect_bluetooth("08:6b:d7:01:de:81")
+    check.read_eeprom_page()
+    check.disconnect()
 
 
 if __name__ == '__main__':
