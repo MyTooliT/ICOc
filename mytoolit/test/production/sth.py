210aa6d0c8c39d69e22a11cc77290cc090fe8f41
René Schwaiger <sanssecours@me.com>
2021-01-20 11:37:17 +0100

    STH Test: Remove Python path modification code

--- mytoolit/test/production/sth.py
+++ mytoolit/test/production/sth.py
@@ -1,14 +1,8 @@
 # -- Imports ------------------------------------------------------------------
 
 from os import environ, pathsep
-from os.path import abspath, dirname
-from sys import path as module_path
 from time import sleep
 from unittest import main as unittest_main, skipIf
-
-# Add path for custom libraries
-repository_root = dirname(dirname(dirname(dirname(abspath(__file__)))))
-module_path.append(repository_root)
 
 from mytoolit.can import Node
 from mytoolit.measurement import (convert_acceleration_adc_to_g,
