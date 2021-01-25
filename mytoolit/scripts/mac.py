46b95cb5c80e62ae68c51608fab2522e85761ad4
René Schwaiger <sanssecours@me.com>
2021-01-25 13:45:41 +0100

    MAC To Base64: Parse arguments with `argparse`

--- mytoolit/scripts/mac.py
+++ mytoolit/scripts/mac.py
@@ -1,39 +1,23 @@
 # -- Imports ------------------------------------------------------------------
 
-from sys import argv, exit, stderr
+from argparse import ArgumentParser
 
+from mytoolit.cmdline import mac_address
 from mytoolit.utility import convert_mac_base64
-
-# -- Functions ----------------------------------------------------------------
-
-
-def usage(command):
-    print(f"Usage: {command} MAC", file=stderr)
-
 
 # -- Main ---------------------------------------------------------------------
 
 
 def main():
-    EXIT_USAGE = 2
+    parser = ArgumentParser(
+        description="Convert a MAC address to an 8 character Base64 text")
+    parser.add_argument("mac",
+                        help="MAC address of STH e.g. 08:6b:d7:01:de:81",
+                        type=mac_address)
+    mac = parser.parse_args().mac
 
-    if len(argv) != 2:
-        usage(argv[0])
-        exit(EXIT_USAGE)
-
-    mac_input = argv[1]
-    mac = mac_input.split(":")
-
-    try:
-        if len(mac) != 6:
-            raise ValueError
-        mac = [int(byte, 16) for byte in mac]
-        name = convert_mac_base64(mac)
-        print(name)
-    except ValueError:
-        print("Please use a MAC address of the form xx:xx:xx:xx:xx:xx",
-              file=stderr)
-        exit(EXIT_USAGE)
+    name = convert_mac_base64([int(byte, 16) for byte in mac.split(":")])
+    print(name)
 
 
 if __name__ == '__main__':
