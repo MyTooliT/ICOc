cfbb61961c8e894ac89fdd73a372dc413da8f075
René Schwaiger <sanssecours@me.com>
2021-02-03 13:36:12 +0100

    CAN: Export new network class

--- mytoolit/can/__init__.py
+++ mytoolit/can/__init__.py
@@ -4,5 +4,6 @@ from .command import Command
 from .identifier import Identifier
 from .message import Message
 from .node import Node
+from .network import Network
 from .status import (NodeStatusSTH, NodeStatusSTU, ErrorStatusSTH,
                      ErrorStatusSTU)
