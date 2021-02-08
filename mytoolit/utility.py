8654b2ee82e2878e7145b529bac573a189d7ccbf
René Schwaiger <sanssecours@me.com>
2021-02-08 08:54:24 +0100

    Utility: Document byte array conversion function

--- mytoolit/utility.py
+++ mytoolit/utility.py
@@ -6,6 +6,25 @@ from base64 import b64encode, b64decode
 
 
 def bytearray_to_text(data):
+    """Convert byte array data to a string
+
+    Please note, that this function ignores non ASCII data and control
+    characters
+
+    Parameters
+    ----------
+
+    data
+        The byte array that should be converted
+
+    Returns
+    -------
+
+    An string where each (valid) byte of the input is mapped to an ASCII
+    character
+
+    """
+
     return bytearray(filter(lambda byte: byte > ord(' ') and byte < 128,
                             data)).decode('ASCII')
 
