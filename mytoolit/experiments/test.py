eb41f1b7356f9fd445ab6a558a8b40d8c6763570
René Schwaiger <sanssecours@me.com>
2021-04-01 17:02:01 +0200

    Experiments: Write and read STH EEPROM text

--- mytoolit/experiments/test.py
+++ mytoolit/experiments/test.py
@@ -9,19 +9,19 @@ async def test(identifier=EUI("08:6b:d7:01:de:81")):
     with Network() as network:
         await network.connect_sth(identifier)
 
-        number = 1337
-        length = 4
-        print(f"Write number: {number}")
-        await network.write_eeprom_int(address=10,
-                                       offset=0,
-                                       value=number,
-                                       length=length,
-                                       node='STH 1')
-        read_number = await network.read_eeprom_int(address=10,
-                                                    offset=0,
-                                                    length=length,
-                                                    node='STH 1')
-        print(f"Read number: {read_number}")
+        text = "something"
+        length = len(text)
+        print(f"Write text: {text}")
+        await network.write_eeprom_text(address=10,
+                                        offset=0,
+                                        text=text,
+                                        length=length,
+                                        node='STH 1')
+        read_text = await network.read_eeprom_text(address=10,
+                                                   offset=0,
+                                                   length=length,
+                                                   node='STH 1')
+        print(f"Read text: {read_text}")
 
 
 if __name__ == '__main__':
