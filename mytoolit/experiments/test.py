11dc5e237166fc9f358203e557bd86eeeb5b6a6e
René Schwaiger <sanssecours@me.com>
2021-04-01 12:39:48 +0200

    Experiments: Read and write EEPROM float value

--- mytoolit/experiments/test.py
+++ mytoolit/experiments/test.py
@@ -8,17 +8,17 @@ from mytoolit.can import Network
 async def test(identifier=EUI("08:6b:d7:01:de:81")):
     with Network() as network:
         await network.connect_sth(identifier)
-        data = [1, 3, 3, 7, 5, 6, 7, 8]
 
-        await network.write_eeprom(address=1,
-                                   offset=0,
-                                   data=data,
-                                   node='STH 1')
-        read_data = await network.read_eeprom(address=1,
-                                              offset=0,
-                                              length=len(data),
-                                              node='STH 1')
-        print(f"Read data: {read_data}")
+        number = 1.337
+        print(f"Write number: {number}")
+        await network.write_eeprom_float(address=10,
+                                         offset=0,
+                                         value=number,
+                                         node='STH 1')
+        read_number = await network.read_eeprom_float(address=10,
+                                                      offset=0,
+                                                      node='STH 1')
+        print(f"Read number: {read_number}")
         await network.deactivate_bluetooth('STU 1')
 
 
