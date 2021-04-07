b46da750eb7c462979f61963386926bf935bbff2
René Schwaiger <sanssecours@me.com>
2021-04-07 12:13:35 +0200

    Experiments: Read MAC address of STU 1

--- mytoolit/experiments/test.py
+++ mytoolit/experiments/test.py
@@ -9,19 +9,9 @@ async def test(identifier=EUI("08:6b:d7:01:de:81")):
     with Network() as network:
         await network.connect_sth(identifier)
 
-        text = "something"
-        length = len(text)
-        print(f"Write text: {text}")
-        await network.write_eeprom_text(address=10,
-                                        offset=0,
-                                        text=text,
-                                        length=length,
-                                        node='STH 1')
-        read_text = await network.read_eeprom_text(address=10,
-                                                   offset=0,
-                                                   length=length,
-                                                   node='STH 1')
-        print(f"Read text: {read_text}")
+        node = 'STU 1'
+        mac_address = await network.get_mac_address(node)
+        print(f"MAC address of {node}: {mac_address}")
 
 
 if __name__ == '__main__':
