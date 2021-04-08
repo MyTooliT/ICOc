35f622bc037d1782b7e163f93e19de0bc7fd9507
René Schwaiger <sanssecours@me.com>
2021-04-08 17:55:42 +0200

    Message: Update representation for connect command

--- mytoolit/can/message.py
+++ mytoolit/can/message.py
@@ -232,9 +232,9 @@ class Message:
                     name = bytearray_to_text(self.data[2:])
                     data_explanation += f": “{name}”"
             elif subcommand == 7:
-                info = ("Acknowledge connection request to"
-                        if is_acknowledgment else "Request connection for")
-                data_explanation = (f"{info} device "
+                info = ("Acknowledge connection request"
+                        if is_acknowledgment else "Request connection")
+                data_explanation = (f"{info} to device "
                                     f"with device number “{device_number}”")
             elif subcommand == 8:
                 data_explanation = f"{verb} Bluetooth connection status"
