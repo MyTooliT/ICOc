25334ae14bc4467f3a3d5f6b41c2be8a54ff1e32
René Schwaiger <sanssecours@me.com>
2021-05-10 13:00:40 +0200

    Experiments: Update context manager statement

--- mytoolit/experiments/test.py
+++ mytoolit/experiments/test.py
@@ -6,12 +6,12 @@ from mytoolit.can import Network
 
 
 async def test(identifier=EUI("08:6b:d7:01:de:81")):
-    with Network() as network:
         await network.connect_sth(identifier)
 
         node = 'STU 1'
         mac_address = await network.get_mac_address(node)
         print(f"MAC address of {node}: {mac_address}")
+    async with Network() as network:
 
 
 if __name__ == '__main__':
