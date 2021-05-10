7f51c6771bf2189959afbb55b95d835494e6b344
René Schwaiger <sanssecours@me.com>
2021-05-10 13:55:33 +0200

    Experiments: Read hardware version

--- mytoolit/experiments/test.py
+++ mytoolit/experiments/test.py
@@ -6,8 +6,8 @@ from mytoolit.can import Network
 async def test():
     async with Network() as network:
         node = 'STU 1'
-        gtin = await network.get_gtin(node)
-        print(f"GTIN of “{node}”: “{gtin}”")
+        hardware_version = await network.get_hardware_version(node)
+        print(f"Hardware version of “{node}”: “{hardware_version}”")
 
 
 if __name__ == '__main__':
