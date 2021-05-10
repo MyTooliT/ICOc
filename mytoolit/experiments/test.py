1f6fd3127a149cd6fee5fa7e66f99ddaffee267e
René Schwaiger <sanssecours@me.com>
2021-05-10 13:25:57 +0200

    Experiments: Read GTIN of STU 1

--- mytoolit/experiments/test.py
+++ mytoolit/experiments/test.py
@@ -1,17 +1,13 @@
 from asyncio import run
 
-from netaddr import EUI
-
 from mytoolit.can import Network
 
 
-async def test(identifier=EUI("08:6b:d7:01:de:81")):
-        await network.connect_sth(identifier)
-
-        node = 'STU 1'
-        mac_address = await network.get_mac_address(node)
-        print(f"MAC address of {node}: {mac_address}")
+async def test():
     async with Network() as network:
+        node = 'STU 1'
+        gtin = await network.get_gtin(node)
+        print(f"GTIN of “{node}”: “{gtin}”")
 
 
 if __name__ == '__main__':
