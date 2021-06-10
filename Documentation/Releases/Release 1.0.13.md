49c9a8f076c1f49459fd928f76498fa45c6fa980
René Schwaiger <sanssecours@me.com>
2021-06-10 12:44:29 +0200

    Streaming: Add class for streaming format

--- Documentation/Releases/Release 1.0.13.md
+++ Documentation/Releases/Release 1.0.13.md
@@ -24,3 +24,7 @@
 ### Network
 
 - Add the coroutine `get_state` to [retrieve information about the current state of a node](https://mytoolit.github.io/Documentation/#command:get-set-state)
+
+### Streaming Format
+
+- New class `StreamingFormat` to specify the format of [streaming data][streaming]
---
+++ mytoolit/can/streaming.py
@@ -0,0 +1,51 @@
+class StreamingFormat:
+    """Support for specifying the streaming format of a node
+
+    See also: https://mytoolit.github.io/Documentation/#block-streaming
+    """
+
+    def __init__(self, *value):
+        """Initialize the streaming format using the given arguments
+
+
+        value:
+            The value of the streaming format byte
+
+        """
+
+        self.value = value[0] if value else 0
+
+    def __repr__(self) -> str:
+        """Retrieve the textual representation of the streaming format
+
+        Returns
+        -------
+
+        A string that describes the streaming format
+
+        Examples
+        --------
+
+        >>> StreamingFormat(0b001)
+        Streaming Format: 1 Data Set
+
+        >>> StreamingFormat(0b111)
+        Streaming Format: 30 Data Sets
+
+        """
+
+        def to_number_data_sets(data_set_bits: int) -> int:
+            return [0, 1, 3, 6, 10, 15, 20, 30][data_set_bits]
+
+        data_set_bits = self.value & 0b111
+        data_sets = to_number_data_sets(data_set_bits)
+
+        return "Streaming Format: {} Data Set{}".format(
+            data_sets, "" if data_sets == 1 else "s")
+
+
+# -- Main ---------------------------------------------------------------------
+
+if __name__ == '__main__':
+    from doctest import testmod
+    testmod()
