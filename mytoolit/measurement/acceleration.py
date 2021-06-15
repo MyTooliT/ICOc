1ea3447892cb4ddaf333e03f8680724642befa9f
René Schwaiger <sanssecours@me.com>
2021-06-15 15:58:10 +0200

    Measurement: Extend comment about acceleration

--- mytoolit/measurement/acceleration.py
+++ mytoolit/measurement/acceleration.py
@@ -30,9 +30,11 @@ def convert_acceleration_adc_to_g(acceleration_raw, max_value):
     max_value_adc = 0xffff
     acceleration_to_gravity = max_value
 
-    # The code (probably) subtracts 1/2 from the computed value, since the STH
-    # linearly maps the maximum negative acceleration to 0 and the maximum
-    # positive acceleration to the maximum ADC value.
+    # The code subtracts 1/2 from the computed value, since the STH linearly
+    # maps the maximum negative acceleration to 0 and the maximum positive
+    # acceleration to the maximum ADC value. Currently the stationary
+    # acceleration value seems to be slightly negative (-g₀ <
+    # acceleration_in_g 0 < 0 ), while in theory it should have a value of g₀.
     acceleration_in_g = (acceleration_raw / max_value_adc -
                          1 / 2) * acceleration_to_gravity
     return acceleration_in_g
