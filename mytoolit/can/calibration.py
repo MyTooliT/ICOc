ac65249319c2cea981ca7ffadb9e9fa1c18ced78
René Schwaiger <sanssecours@me.com>
2021-06-16 16:30:04 +0200

    Calibration: Add support for method

--- mytoolit/can/calibration.py
+++ mytoolit/can/calibration.py
@@ -6,7 +6,9 @@ class CalibrationMeasurementFormat:
 
     """
 
-    def __init__(self, set: bool) -> None:
+    methods = ['Reserved', 'Activate', 'Deactivate', 'Measure']
+
+    def __init__(self, set: bool, method: str) -> None:
         """Initialize the calibration measurement format
 
         Parameters
@@ -16,9 +18,17 @@ class CalibrationMeasurementFormat:
             Specifies if we want to set or retrieve (get) calibration
             measurement data
 
+        method:
+            Specifies the calibration method (Activate, Deactivate or Measure)
+
         """
 
-        method_byte = int(set) << 7
+        cls = type(self)
+
+        if method not in cls.methods:
+            raise ValueError(f"Unknown method “{method}”")
+
+        method_byte = (int(set) << 7) | cls.methods.index(method) << 5
 
         self.data = [method_byte, 0, 0]
 
@@ -34,18 +44,24 @@ class CalibrationMeasurementFormat:
         Examples
         --------
 
-        >>> CalibrationMeasurementFormat(set=False)
-        Get
+        >>> CalibrationMeasurementFormat(set=False, method='Activate')
+        Get, Activate
 
-        >>> CalibrationMeasurementFormat(set=True)
-        Set
+        >>> CalibrationMeasurementFormat(set=True, method='Measure')
+        Set, Measure
 
         """
 
-        method_byte = self.data[0]
-        set = method_byte >> 7
+        cls = type(self)
 
-        return "Set" if set else "Get"
+        method_byte = self.data[0]
+
+        set = method_byte >> 7
+        method = cls.methods[(method_byte >> 5) & 0b11]
+
+        representation = ", ".join(["Set" if set else "Get", method])
+
+        return representation
 
 
 # -- Main ---------------------------------------------------------------------
