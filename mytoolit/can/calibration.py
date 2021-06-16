d12b3328cf3f436ac6cada4b6d1eba177f630c72
René Schwaiger <sanssecours@me.com>
2021-06-16 17:26:40 +0200

    Calibration: Add support for setting element

--- mytoolit/can/calibration.py
+++ mytoolit/can/calibration.py
@@ -1,3 +1,10 @@
+# -- Imports ------------------------------------------------------------------
+
+from bidict import bidict
+
+# -- Class --------------------------------------------------------------------
+
+
 class CalibrationMeasurementFormat:
     """This class stores message data for the calibration measurement command
 
@@ -7,8 +14,20 @@ class CalibrationMeasurementFormat:
     """
 
     methods = ['Reserved', 'Activate', 'Deactivate', 'Measure']
+    elements = bidict({
+        "Acceleration": 0,
+        "Temperature": 1,
+        "Voltage": 32,
+        "VSS": 96,
+        "VDD": 97,
+        "Internal": 98,
+        "OPV": 99,
+    })
 
-    def __init__(self, set: bool, method: str) -> None:
+    def __init__(self,
+                 set: bool,
+                 element: str,
+                 method: str = 'Reserved') -> None:
         """Initialize the calibration measurement format
 
         Parameters
@@ -16,7 +35,21 @@ class CalibrationMeasurementFormat:
 
         set:
             Specifies if we want to set or retrieve (get) calibration
-            measurement data
+            measurement data. If you set this value to false (get), then
+            the value of `method` will be ignored
+
+        element:
+            Specifies the element that should be measured
+
+            Possible values:
+
+                - Acceleration
+                - Temperature
+                - Voltage
+                - VSS
+                - VDD
+                - Internal
+                - OPV
 
         method:
             Specifies the calibration method (Activate, Deactivate or Measure)
@@ -28,9 +61,12 @@ class CalibrationMeasurementFormat:
         if method not in cls.methods:
             raise ValueError(f"Unknown method “{method}”")
 
-        method_byte = (int(set) << 7) | cls.methods.index(method) << 5
+        if element not in cls.elements:
+            raise ValueError(f"Unknown element “{element}”")
 
-        self.data = [method_byte, 0, 0]
+        method_byte = (int(set) << 7) | cls.methods.index(method) << 5
+        element_byte = cls.elements[element]
+        self.data = [method_byte, element_byte, 0]
 
     def __repr__(self) -> str:
         """
@@ -44,11 +80,12 @@ class CalibrationMeasurementFormat:
         Examples
         --------
 
-        >>> CalibrationMeasurementFormat(set=False, method='Activate')
-        Get, Activate
+        >>> CalibrationMeasurementFormat(set=False, element='Acceleration')
+        Get, Acceleration
 
-        >>> CalibrationMeasurementFormat(set=True, method='Measure')
-        Set, Measure
+        >>> CalibrationMeasurementFormat(set=True, method='Measure',
+        ...     element='Temperature')
+        Set, Measure, Temperature
 
         """
 
@@ -57,9 +94,13 @@ class CalibrationMeasurementFormat:
         method_byte = self.data[0]
 
         set = method_byte >> 7
-        method = cls.methods[(method_byte >> 5) & 0b11]
+        element = cls.elements.inverse[self.data[1]]
+        parts = ["Set" if set else "Get", element]
+        if set:
+            method = cls.methods[(method_byte >> 5) & 0b11]
+            parts.insert(1, method)
 
-        representation = ", ".join(["Set" if set else "Get", method])
+        representation = ", ".join(parts)
 
         return representation
 
