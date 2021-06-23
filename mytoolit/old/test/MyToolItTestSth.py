3355cf8bda29672b5e729fbbe986be99c4ff1543
René Schwaiger <sanssecours@me.com>
2021-06-23 12:16:30 +0200

    Test: Fix STH limits intialization

--- mytoolit/old/test/MyToolItTestSth.py
+++ mytoolit/old/test/MyToolItTestSth.py
@@ -38,8 +38,7 @@ This class is used for automated internal verification of the sensory tool holde
 
 class TestSth(unittest.TestCase):
     def setUp(self):
-        self.tSthLimits = SthLimits(iSensorAxis, bBattery, uAdc2Acc, iRssiMin,
-                                    20, 35)
+        self.tSthLimits = SthLimits(iSensorAxis, bBattery, uAdc2Acc, iRssiMin)
         self.sHomeLocation = sHomeLocation
         self.sBuildLocation = sHomeLocation + "builds/" + sVersion
         self.sBootloader = sHomeLocation + "builds/" + "BootloaderOtaBgm113.s37"
