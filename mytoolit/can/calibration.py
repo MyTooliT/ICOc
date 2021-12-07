73bf933820bfa427c50d46387a6c9d9c750199fc
René Schwaiger <sanssecours@me.com>
2021-12-07 15:37:13 +0100

    Calibration: Add measurement dimension

--- mytoolit/can/calibration.py
+++ mytoolit/can/calibration.py
@@ -27,7 +27,8 @@ class CalibrationMeasurementFormat:
     def __init__(self,
                  set: bool,
                  element: str,
-                 method: str = 'Reserved') -> None:
+                 method: str = 'Reserved',
+                 dimension: int = 1) -> None:
         """Initialize the calibration measurement format
 
         Parameters
@@ -54,6 +55,10 @@ class CalibrationMeasurementFormat:
         method:
             Specifies the calibration method (Activate, Deactivate or Measure)
 
+        dimension:
+            Specifies the measurement dimension respectively the axis
+            (1, 2 or 3)
+
         """
 
         cls = type(self)
@@ -64,9 +69,12 @@ class CalibrationMeasurementFormat:
         if element not in cls.elements:
             raise ValueError(f"Unknown element “{element}”")
 
+        if dimension not in {1, 2, 3}:
+            raise ValueError(f"Unknown dimension “{dimension}”")
+
         method_byte = (int(set) << 7) | cls.methods.index(method) << 5
         element_byte = cls.elements[element]
-        self.data = [method_byte, element_byte, 0]
+        self.data = [method_byte, element_byte, dimension]
 
     def __repr__(self) -> str:
         """
@@ -81,11 +89,11 @@ class CalibrationMeasurementFormat:
         --------
 
         >>> CalibrationMeasurementFormat(set=False, element='Acceleration')
-        Get, Acceleration
+        Get, Acceleration, Dimension: 1
 
         >>> CalibrationMeasurementFormat(set=True, method='Measure',
-        ...     element='Temperature')
-        Set, Measure, Temperature
+        ...     element='Temperature', dimension=2)
+        Set, Measure, Temperature, Dimension: 2
 
         """
 
@@ -95,7 +103,9 @@ class CalibrationMeasurementFormat:
 
         set = method_byte >> 7
         element = cls.elements.inverse[self.data[1]]
-        parts = ["Set" if set else "Get", element]
+        dimension = self.data[2]
+
+        parts = ["Set" if set else "Get", element, f"Dimension: {dimension}"]
         if set:
             method = cls.methods[(method_byte >> 5) & 0b11]
             parts.insert(1, method)
