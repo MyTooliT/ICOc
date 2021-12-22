178fbaf00ea0d3c6583cd8f311d9d046e3719812
René Schwaiger <sanssecours@me.com>
2021-12-22 14:52:44 +0100

    Package: Export network exceptions

--- mytoolit/can/__init__.py
+++ mytoolit/can/__init__.py
@@ -4,6 +4,7 @@ from .command import Command
 from .identifier import Identifier
 from .message import Message
 from .node import Node
-from .network import Network
+from .network import (Network, ErrorResponseError, NetworkError,
+                      NoResponseError)
 from .status import (NodeStatusSTH, NodeStatusSTU, ErrorStatusSTH,
                      ErrorStatusSTU, State)
