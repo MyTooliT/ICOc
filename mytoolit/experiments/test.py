e5ea1f18e3360b7556b2627b366cc24241869425
René Schwaiger <sanssecours@me.com>
2021-12-22 15:01:07 +0100

    Experiments: Read supply voltage

--- mytoolit/experiments/test.py
+++ mytoolit/experiments/test.py
@@ -3,34 +3,28 @@ from time import time
 
 from netaddr import EUI
 
-from mytoolit.can import Network
+from mytoolit.can import Network, NetworkError
 
 
 async def test(identifier=EUI("08:6b:d7:01:de:81")):
     async with Network() as network:
-        node = 'STH 1'
         start_time = time()
 
+        print("Try to read supply voltage")
+
         await network.connect_sth(identifier)
-        name = await network.get_name(node)
-        print(f"Name of {node}: {name}")
 
-        voltage = await network.read_acceleration_voltage()
-        print(f"Acceleration Voltage Before: {voltage} V")
+        try:
+            supply_voltage = await network.read_supply_voltage()
+            print(f"Supply voltage: {supply_voltage:.3f}")
+        except NetworkError:
+            error = "Unable to read supply voltage"
+            if not await network.is_connected():
+                print(f"{error} because of disconnect")
+            else:
+                print(f"{error} due to unknown reason")
 
-        print("Activate self test")
-        await network.activate_acceleration_self_test()
-
-        voltage = await network.read_acceleration_voltage()
-        print(f"Acceleration Voltage Between: {voltage} V")
-
-        print("Deactivate self test")
-        await network.deactivate_acceleration_self_test()
-
-        voltage = await network.read_acceleration_voltage()
-        print(f"Acceleration Voltage After: {voltage} V")
-
-        print("\nExecution took {:.3} seconds".format(time() - start_time))
+        print("Execution took {:.3} seconds\n".format(time() - start_time))
 
 
 if __name__ == '__main__':
