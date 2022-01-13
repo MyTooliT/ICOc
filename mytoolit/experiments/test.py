ed7c6a9ac1573c8615ee8994b86781fcf07bd346
René Schwaiger <sanssecours@me.com>
2022-01-13 14:02:41 +0100

    Experiments: Read energy reduced mode times

--- mytoolit/experiments/test.py
+++ mytoolit/experiments/test.py
@@ -3,28 +3,23 @@ from time import time
 
 from netaddr import EUI
 
-from mytoolit.can import Network, NetworkError
+from mytoolit.can import Network
 
 
 async def test(identifier=EUI("08:6b:d7:01:de:81")):
     async with Network() as network:
+        node = 'STH 1'
         start_time = time()
 
-        print("Try to read supply voltage")
-
         await network.connect_sth(identifier)
+        name = await network.get_name(node)
+        print(f"Name of {node}: {name}\n")
 
-        try:
-            supply_voltage = await network.read_supply_voltage()
-            print(f"Supply voltage: {supply_voltage:.3f}")
-        except NetworkError:
-            error = "Unable to read supply voltage"
-            if not await network.is_connected():
-                print(f"{error} because of disconnect")
-            else:
-                print(f"{error} due to unknown reason")
+        times = await network.read_energy_mode_reduced()
+        print(f"Sleep Time:         {times.sleep} ms")
+        print(f"Advertisement Time: {times.advertisement} ms")
 
-        print("Execution took {:.3} seconds\n".format(time() - start_time))
+        print("\nExecution took {:.3} seconds".format(time() - start_time))
 
 
 if __name__ == '__main__':
