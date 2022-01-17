c96ebe31a5c1ec41b805d279c7fa2f8d67fc681b
René Schwaiger <sanssecours@me.com>
2022-01-17 16:06:54 +0100

    Experiments: Read acceleration data

--- mytoolit/experiments/test.py
+++ mytoolit/experiments/test.py
@@ -1,14 +1,25 @@
-from mytoolit.config import settings
-from mytoolit.cmdline.commander import Commander, CommanderException
+from asyncio import run
+from time import time
+
+from netaddr import EUI
+
+from mytoolit.can import Network
+
+
+async def test(identifier=EUI("08:6b:d7:01:de:81")):
+    async with Network() as network:
+        node = 'STH 1'
+        start_time = time()
+
+        await network.connect_sth(identifier)
+        name = await network.get_name(node)
+        print(f"Name of {node}: {name}")
+
+        print("Read acceleration data")
+        await network.read_acceleration()
+
+        print("\nExecution took {:.3} seconds".format(time() - start_time))
+
 
 if __name__ == '__main__':
-    commander = Commander(
-        serial_number=settings.sth.programming_board.serial_number,
-        chip='BGM113A256V2')
-    try:
-        commander.enable_debug_mode()
-        print("Successfully enabled debug mode for STH")
-        commander.unlock_device()
-        print("Successfully unlocked STH")
-    except CommanderException as exception:
-        print(str(exception))
+    run(test())
