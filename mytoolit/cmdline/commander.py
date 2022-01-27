475cfcabb3155aa6d9386b77c79502047c8158bd
René Schwaiger <sanssecours@me.com>
2022-01-27 14:01:08 +0100

    Commander: Add method to read power usage

--- mytoolit/cmdline/commander.py
+++ mytoolit/cmdline/commander.py
@@ -1,11 +1,17 @@
 # -- Imports ------------------------------------------------------------------
 
 from os import environ, pathsep
+from re import compile
+from subprocess import run
 from sys import platform
 
 from mytoolit.config import settings
 
-# -- Class --------------------------------------------------------------------
+# -- Classes ------------------------------------------------------------------
+
+
+class CommanderException(Exception):
+    """Describes problems regarding the execution of Simplicity Commander"""
 
 
 class Commander:
@@ -38,6 +44,69 @@ class Commander:
         path = settings.commands.path
         paths = path.linux if platform == 'Linux' else path.windows
         environ['PATH'] += (pathsep + pathsep.join(paths))
+
+    def read_power_usage(self,
+                         serial_number: int,
+                         chip: str,
+                         milliseconds: float = 1000) -> float:
+        """Read the power usage of the connected hardware
+
+        Parameters
+        ----------
+
+        serial_number:
+            The serial number of the programming board that is connected to
+            the hardware
+
+        chip:
+            The identifier of the chip on the PCB e.g. “BGM121A256V2”
+
+        milliseconds:
+            The amount of time the power usage should be measured for
+
+        Returns
+        -------
+
+        The measured power usage in milliwatts
+
+        Example
+        -------
+
+        Measure power usage of connected STH
+
+        >>> from mytoolit.config import settings
+
+        >>> serial_number = settings.sth.programming_board.serial_number
+        >>> chip = 'BGM121A256V2'
+        >>> Commander().read_power_usage(serial_number, chip) > 0
+        True
+
+        """
+
+        identification_arguments = [
+            "--serialno",
+            f"{serial_number}",
+            "-d",
+            chip,
+        ]
+
+        command = [
+            "commander", "aem", "measure", "--windowlength",
+            str(milliseconds)
+        ] + identification_arguments
+
+        result = run(command, capture_output=True, text=True)
+
+        regex = compile(r"Power\s*\[mW\]\s*:\s*(?P<milliwatts>\d+\.\d+)")
+        pattern_match = regex.search(result.stdout)
+        if pattern_match is None:
+            raise CommanderException("Unable to extract power usage "
+                                     "from Simplicity Commander output")
+
+        assert pattern_match is not None
+        milliwatts = pattern_match['milliwatts']
+
+        return float(milliwatts)
 
 
 # -- Main ---------------------------------------------------------------------
