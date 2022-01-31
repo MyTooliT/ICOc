e520037d7aadaafcaf916e32256722bb40c04be8
René Schwaiger <sanssecours@me.com>
2022-01-31 14:32:27 +0100

    Experiments: Test code to enable debug mode

--- mytoolit/experiments/test.py
+++ mytoolit/experiments/test.py
@@ -1,9 +1,12 @@
 from mytoolit.config import settings
-from mytoolit.cmdline.commander import Commander
+from mytoolit.cmdline.commander import Commander, CommanderException
 
 if __name__ == '__main__':
     commander = Commander(
         serial_number=settings.sth.programming_board.serial_number,
         chip='BGM113A256V2')
-    power_usage = commander.read_power_usage()
-    print(f"Power Usage: {power_usage} mW")
+    try:
+        commander.enable_debug_mode()
+        print("Successfully enabled debug mode for STH")
+    except CommanderException as exception:
+        print(str(exception))
