e8150e4b5c0657ef98899f260bf1f02b194c93a8
René Schwaiger <sanssecours@me.com>
2022-01-31 13:00:14 +0100

    Experiments: Test power usage read method

--- mytoolit/experiments/test.py
+++ mytoolit/experiments/test.py
@@ -1,28 +1,9 @@
-from asyncio import run
-from time import time
-
-from netaddr import EUI
-
-from mytoolit.can import Network
-
-
-async def test(identifier):
-    async with Network() as network:
-        node = 'STH 1'
-        start_time = time()
-
-        await network.connect_sth(identifier)
-        name = await network.get_name(node)
-        print(f"Name of {node}: {name}\n")
-
-        values = await network.read_sensor_values()
-        print(f"Sensor Values: {values}")
-
-        print(f"\nExecution took {time() - start_time:.3f} seconds")
-
+from mytoolit.config import settings
+from mytoolit.cmdline.commander import Commander
 
 if __name__ == '__main__':
-    sth_mac = EUI("08:6b:d7:01:de:81")
-    smh_mac = EUI("94-DE-B8-ED-70-37")
-
-    run(test(identifier=smh_mac))
+    commander = Commander(
+        serial_number=settings.sth.programming_board.serial_number,
+        chip='BGM113A256V2')
+    power_usage = commander.read_power_usage()
+    print(f"Power Usage: {power_usage} mW")
