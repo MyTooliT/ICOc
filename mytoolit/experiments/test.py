5bc2784b183bb795d856df46b59daaf9505d6392
René Schwaiger <sanssecours@me.com>
2022-02-08 12:54:33 +0100

    Experiments: Read STH reduced energy time values

--- mytoolit/experiments/test.py
+++ mytoolit/experiments/test.py
@@ -3,7 +3,7 @@ from time import time
 
 from netaddr import EUI
 
-from mytoolit.can.network import Network, Times
+from mytoolit.can.network import Network
 
 
 async def test(identifier=EUI("08:6b:d7:01:de:81")):
@@ -15,19 +15,7 @@ async def test(identifier=EUI("08:6b:d7:01:de:81")):
         name = await network.get_name(node)
         print(f"Name of {node}: {name}\n")
 
-        times = Times(
-            sleep=300000,
-            advertisement=1250,
-        )
-        node = 'STU 1'
-        device_number = 0xff
-        print(f"Set time values for “{node}” (device number: {device_number}) "
-              f" to “{times}”\n")
-        await network.write_energy_mode_reduced(node=node, times=times)
-
-        times = await network.read_energy_mode_reduced(
-            node=node, device_number=device_number)
-        print(f"{node}\n{'—' * len(node)}")
+        times = await network.read_energy_mode_reduced(node=node)
         print(f"Advertisement Time: {times.advertisement} ms")
         print(f"Sleep Time:         {times.sleep} ms")
 
