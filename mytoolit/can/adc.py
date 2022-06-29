f2f6dfbcff6d31a97525440b33faf5f11c9afddd
René Schwaiger <sanssecours@me.com>
2022-06-29 13:07:53 +0200

    ADC: Add prescaler parameter

--- mytoolit/can/adc.py
+++ mytoolit/can/adc.py
@@ -10,7 +10,8 @@ class ADCConfiguration:
 
     def __init__(self,
                  *data: Union[bytearray, List[int]],
-                 set: Optional[bool] = None):
+                 set: Optional[bool] = None,
+                 prescaler: int = 1):
         """Initialize the ADC configuration using the given arguments
 
         Positional Parameters
@@ -24,6 +25,9 @@ class ADCConfiguration:
 
         set:
             Specifies if we want to set or retrieve (get) the ADC configuration
+
+        prescaler:
+            The ADC prescaler value (1 – 127)
 
         """
 
@@ -39,7 +43,11 @@ class ADCConfiguration:
                                  f"“{required_length}” required")
             self.data = data_bytes[0:5] + [0] * 3
         else:
-            self.data = [0] * 8
+            self.data = [0, 1] + [0] * 6
+
+        # ==================
+        # = Get/Set Config =
+        # ==================
 
         if set is not None:
             get_set_byte = self.data[0]
@@ -48,6 +56,15 @@ class ADCConfiguration:
             # Set value
             get_set_byte |= int(set) << 7
             self.data[0] = get_set_byte
+
+        # =============
+        # = Prescaler =
+        # =============
+
+        if not (1 <= prescaler <= 127):
+            raise ValueError(f"Prescaler value of “{prescaler}” out of range"
+                             ", please use a value between 1 and 127")
+        self.data[1] = prescaler
 
     def __repr__(self) -> str:
         """Retrieve the textual representation of the ADC configuration
@@ -61,15 +78,19 @@ class ADCConfiguration:
         --------
 
         >>> ADCConfiguration()
-        Get
+        Get, Prescaler: 1
 
-        >>> ADCConfiguration(set=True)
-        Set
+        >>> ADCConfiguration(set=True, prescaler=64)
+        Set, Prescaler: 64
 
         """
 
         set = bool(self.data[0] >> 7)
-        return "Set" if set else "Get"
+        prescaler = self.data[1]
+
+        parts = ["Set" if set else "Get", f"Prescaler: {prescaler}"]
+
+        return ", ".join(parts)
 
 
 # -- Main ---------------------------------------------------------------------
