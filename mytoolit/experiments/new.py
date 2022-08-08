b3c8b445a1457641904c4c8d4604f98480917f95
René Schwaiger <sanssecours@me.com>
2022-08-08 16:43:03 +0200

    Experiments: Read sensor configuration

--- mytoolit/experiments/new.py
+++ mytoolit/experiments/new.py
@@ -6,29 +6,12 @@ from asyncio import run
 from time import time
 
 from mytoolit.can import Network
+from mytoolit.can.error import UnsupportedFeatureException
 
 
 # -- Functions ----------------------------------------------------------------
 async def test(identifier="Test-STH"):
     async with Network() as network:
-
-        async def read_supply_voltage(reference_voltage: float):
-            await network.write_adc_configuration(
-                prescaler=2,
-                acquisition_time=8,
-                oversampling_rate=64,
-                reference_voltage=reference_voltage)
-
-            adc_config = await network.read_adc_configuration()
-            reference_voltage_read = adc_config.reference_voltage()
-
-            if reference_voltage_read != reference_voltage:
-                raise Exception(
-                    f"Written reference voltage {reference_voltage} V "
-                    f"and read reference voltage {reference_voltage_read} V "
-                    "are different")
-
-            return await network.read_supply_voltage()
 
         node = 'STH 1'
         start_time = time()
@@ -39,10 +22,11 @@ async def test(identifier="Test-STH"):
         print(f"Connected to sensor device “{name}” with MAC "
               f"address “{mac_address}”\n")
 
-        for reference_voltage in (1.8, 3.3):
-            supply_voltage = await read_supply_voltage(reference_voltage)
-            print(f"Supply Voltage (Reference: {reference_voltage} V):",
-                  f"{supply_voltage} V")
+        try:
+            sensor_config = await network.read_sensor_configuration()
+            print(f"Sensor Configuration: {sensor_config}")
+        except UnsupportedFeatureException as error:
+            print(error)
 
         print("\nExecution took {:.3} seconds".format(time() - start_time))
 
