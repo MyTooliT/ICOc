f77de032dfcdde415b992117410936e41d135558
René Schwaiger <sanssecours@me.com>
2022-08-17 17:00:55 +0200

    Experiments: Check self test of STH

--- mytoolit/experiments/new.py
+++ mytoolit/experiments/new.py
@@ -5,34 +5,52 @@
 from asyncio import run
 
 from mytoolit.can import Network
-from mytoolit.measurement import convert_acceleration_adc_to_g
 
 
 # -- Functions ----------------------------------------------------------------
-async def test(identifier="Test-STH"):
-    network = Network()
-    node = 'STH 1'
+async def test(identifier):
+    async with Network() as network:
+        node = 'STH 1'
 
-    await network.connect_sensor_device(identifier)
-    name = await network.get_name(node)
-    mac_address = await network.get_mac_address(node)
-    print(f"Connected to sensor device “{name}” with MAC "
-          f"address “{mac_address}”\n")
+        await network.connect_sensor_device(identifier)
+        name = await network.get_name(node)
+        mac_address = await network.get_mac_address(node)
+        print(f"Connected to sensor device “{name}” with MAC "
+              f"address “{mac_address}”")
 
-    values_raw = await network.read_x_acceleration_raw(seconds=1)
-    max_value = int(
-        abs(await network.read_eeprom_x_axis_acceleration_offset()) * 2)
-    values_in_g = [
-        convert_acceleration_adc_to_g(value, max_value=max_value)
-        for value in values_raw
-    ]
+        for reference_voltage in (1.8, 3.3):
+            await network.write_adc_configuration(
+                reference_voltage=reference_voltage)
+            adc_config = await network.read_adc_configuration()
+            reference_voltage = adc_config.reference_voltage()
+            print(f"\nReference Voltage: {reference_voltage} V")
 
-    print(f"Values: {values_in_g}")
+            for dimension in list("xyz"):
 
-    await network.shutdown()  # Also call this in case of error
+                acceleration_voltage_before = (
+                    await network.read_acceleration_voltage(
+                        dimension=dimension,
+                        reference_voltage=reference_voltage))
+
+                await network.activate_acceleration_self_test()
+
+                acceleration_voltage = await network.read_acceleration_voltage(
+                    dimension=dimension, reference_voltage=reference_voltage)
+
+                await network.deactivate_acceleration_self_test()
+
+                difference_voltage = (acceleration_voltage -
+                                      acceleration_voltage_before)
+                difference_voltage_mv = round(difference_voltage * 1000)
+
+                print(f"{dimension.upper()} Difference: "
+                      f"{difference_voltage_mv} mV")
 
 
 # -- Main ---------------------------------------------------------------------
 
 if __name__ == '__main__':
-    run(test())
+    new_sth = ""
+    old_sth = "Test-STH"
+
+    run(test(new_sth))
