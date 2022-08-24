49e29eb1ca821505ff9f3e848178462aa7dae87a
René Schwaiger <sanssecours@me.com>
2022-08-24 13:10:15 +0200

    Experiments: Write and read y-axis slope value

--- mytoolit/experiments/new.py
+++ mytoolit/experiments/new.py
@@ -18,11 +18,11 @@ async def test(identifier):
         print(f"Connected to sensor device “{name}” with MAC "
               f"address “{mac_address}”")
 
-        slope = 1.2345
-        print(f"Store slope value y-axis: {slope}")
-        await network.write_eeprom_y_axis_acceleration_slope(1.2345)
-        slope = await network.read_eeprom_y_axis_acceleration_slope()
-        print(f"Slope y-axis: {slope}")
+        offset = 10.4
+        print(f"Store offset value y-axis: {offset}")
+        await network.write_eeprom_y_axis_acceleration_offset(offset)
+        offset = await network.read_eeprom_y_axis_acceleration_offset()
+        print(f"Slope y-axis: {offset}")
 
 
 # -- Main ---------------------------------------------------------------------
