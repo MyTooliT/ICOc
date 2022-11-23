85a2c2d4bb25a32e7d90d8f2476af5757abb65e6
René Schwaiger <sanssecours@me.com>
2022-11-23 12:29:08 +0100

    ICOn: Wait until number of devices stays the same

--- mytoolit/cmdline/icon.py
+++ mytoolit/cmdline/icon.py
@@ -70,11 +70,17 @@ async def list_sensor_devices() -> None:
     """Print a list of available sensor devices"""
 
     async with Network() as network:
-        # - First request for sensor devices will produce empty list
-        # - Subsequent retries should provide all available sensor devices
         timeout = time() + 5
         sensor_devices: List[STHDeviceInfo] = []
-        while len(sensor_devices) <= 0 and time() < timeout:
+        sensor_devices_before: List[STHDeviceInfo] = []
+
+        # - First request for sensor devices will produce empty list
+        # - Subsequent retries should provide all available sensor devices
+        # - We wait until the number of sensor devices is larger than 1 and
+        #   has not changed between one iteration or the timeout is reached
+        while (len(sensor_devices) <= 0 and time() < timeout
+               or len(sensor_devices) != len(sensor_devices_before)):
+            sensor_devices_before = list(sensor_devices)
             sensor_devices = await network.get_sensor_devices()
             await sleep(0.5)
 
