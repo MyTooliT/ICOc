4b260bcee23e72bb8191108b9cf534bb6b799206
René Schwaiger <sanssecours@me.com>
2023-03-01 15:20:58 +0100

    Experiments: Store streaming data in HDF5 format

--- mytoolit/experiments/new.py
+++ mytoolit/experiments/new.py
@@ -4,9 +4,11 @@
 
 from asyncio import run
 from functools import partial
+from pathlib import Path
+from time import time
 
 from mytoolit.can import Network
-from mytoolit.measurement import convert_raw_to_g
+from mytoolit.measurement import convert_raw_to_g, Storage
 
 
 # -- Functions ----------------------------------------------------------------
@@ -22,13 +24,33 @@ async def test(identifier):
             f"address “{mac_address}”"
         )
 
-        stream_data = await network.read_streaming_data_amount(1000)
-
         sensor_range = await network.read_acceleration_sensor_range_in_g()
-        conversion_to_g = partial(convert_raw_to_g, max_value=sensor_range)
-        stream_data.apply(conversion_to_g)
+        print(f"Sensor Range: {sensor_range/2} g")
 
-        print(f"\nStream Data:\n{stream_data}")
+        conversion_to_g = partial(convert_raw_to_g, max_value=sensor_range)
+
+        measurement_time_s = 5 * 60
+
+        print(f"Measure acceleration values for {measurement_time_s} seconds…")
+        filepath = Path(__file__).parent.parent.parent / "experiment.hdf5"
+        with Storage(filepath) as storage:
+            start_time = time()
+            async with network.open_data_stream(first=True) as stream:
+                async for data in stream:
+                    data.apply(conversion_to_g)
+                    x_acceleration_values = data.first
+                    for timestamped in x_acceleration_values:
+                        storage.add_acceleration(
+                            values={"x": timestamped.value.magnitude},
+                            counter=timestamped.counter,
+                            timestamp=timestamped.timestamp,
+                        )
+
+                    if time() - start_time >= measurement_time_s:
+                        break
+                storage.add_acceleration_meta(
+                    "Sensor_Range", f"± {sensor_range} g₀"
+                )
 
 
 # -- Main ---------------------------------------------------------------------
