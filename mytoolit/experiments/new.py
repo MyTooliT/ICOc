6a4cb145132f635cdc7a6d2173d4caad1e3d522e
René Schwaiger <sanssecours@me.com>
2023-04-03 11:10:24 +0200

    Experiments: Print streaming data timestamp

--- mytoolit/experiments/new.py
+++ mytoolit/experiments/new.py
@@ -3,15 +3,13 @@
 # -- Imports ------------------------------------------------------------------
 
 from asyncio import run
-from functools import partial
-from pathlib import Path
-from time import time
+from datetime import datetime
 
 from mytoolit.can import Network
-from mytoolit.measurement import convert_raw_to_g, Storage
-
 
 # -- Functions ----------------------------------------------------------------
+
+
 async def test(identifier):
     async with Network() as network:
         node = "STH 1"
@@ -27,31 +25,15 @@ async def test(identifier):
         sensor_range = await network.read_acceleration_sensor_range_in_g()
         print(f"Sensor Range: {sensor_range/2} g")
 
-        conversion_to_g = partial(convert_raw_to_g, max_value=sensor_range)
+        data = await network.read_streaming_data_single()
+        timestamp = data.first.pop().timestamp
+        timestamp_utc = datetime.utcfromtimestamp(timestamp)
+        timestamp_local = datetime.fromtimestamp(timestamp)
 
-        measurement_time_s = 5 * 60
-
-        print(f"Measure acceleration values for {measurement_time_s} seconds…")
-        filepath = Path(__file__).parent.parent.parent / "experiment.hdf5"
-        with Storage(filepath) as storage:
-            start_time = time()
-            async with network.open_data_stream(first=True) as stream:
-                async for data in stream:
-                    data.apply(conversion_to_g)
-                    x_acceleration_values = data.first
-                    for timestamped in x_acceleration_values:
-                        timestamp_ms = timestamped.timestamp * 1000
-                        storage.add_acceleration(
-                            values={"x": timestamped.value.magnitude},
-                            counter=timestamped.counter,
-                            timestamp=timestamp_ms,
-                        )
-
-                    if time() - start_time >= measurement_time_s:
-                        break
-                storage.add_acceleration_meta(
-                    "Sensor_Range", f"± {sensor_range} g₀"
-                )
+        print(f"Timestamp:     {timestamp}")
+        print(f"Timestamp UTC: {timestamp_utc.timestamp()}")
+        print(f"Local Time:    {timestamp_local.isoformat()}")
+        print(f"UTC Time:      {timestamp_utc.isoformat()}")
 
 
 # -- Main ---------------------------------------------------------------------
