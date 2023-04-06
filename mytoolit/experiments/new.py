d9f0fd60a35d163fe81f321186ecd2c6fd8b1dbc
René Schwaiger <sanssecours@me.com>
2023-04-06 16:10:50 +0200

    Experiments: Store streaming data in HDF 5 file

--- mytoolit/experiments/new.py
+++ mytoolit/experiments/new.py
@@ -3,9 +3,14 @@
 # -- Imports ------------------------------------------------------------------
 
 from asyncio import run
-from datetime import datetime
+from functools import partial
+from pathlib import Path
+from time import time
 
 from mytoolit.can import Network
+from mytoolit.can.adc import ADCConfiguration
+from mytoolit.measurement import convert_raw_to_g, Storage
+
 
 # -- Functions ----------------------------------------------------------------
 
@@ -22,18 +27,43 @@ async def test(identifier):
             f"address “{mac_address}”"
         )
 
+        # Reduce sample rate to decrease CPU usage
+        # See: https://github.com/MyTooliT/ICOc/issues/40
+        adc_config = ADCConfiguration(
+            reference_voltage=3.3,
+            prescaler=16,
+            acquisition_time=8,
+            oversampling_rate=64,
+        )
+        sample_rate = adc_config.sample_rate()
+        print(f"Set sample rate to {sample_rate} Hz")
+        await network.write_adc_configuration(**adc_config)
+
         sensor_range = await network.read_acceleration_sensor_range_in_g()
         print(f"Sensor Range: {sensor_range/2} g")
 
-        data = await network.read_streaming_data_single()
-        timestamp = data.first.pop().timestamp
-        timestamp_utc = datetime.utcfromtimestamp(timestamp)
-        timestamp_local = datetime.fromtimestamp(timestamp)
+        conversion_to_g = partial(convert_raw_to_g, max_value=sensor_range)
 
-        print(f"Timestamp:     {timestamp}")
-        print(f"Timestamp UTC: {timestamp_utc.timestamp()}")
-        print(f"Local Time:    {timestamp_local.isoformat()}")
-        print(f"UTC Time:      {timestamp_utc.isoformat()}")
+        measurement_time_s = 10
+
+        print(f"Measure acceleration values for {measurement_time_s} seconds…")
+        filepath = Path(__file__).parent.parent.parent / "experiment.hdf5"
+        if filepath.is_file():
+            filepath.unlink()  # Remove old HDF file
+        with Storage(filepath) as storage:
+            start_time = time()
+            async with network.open_data_stream(
+                first=True, third=True
+            ) as stream:
+                async for data in stream:
+                    data.apply(conversion_to_g)
+                    storage.add_streaming_data(data)
+
+                    if time() - start_time >= measurement_time_s:
+                        break
+            storage.add_acceleration_meta(
+                "Sensor_Range", f"± {sensor_range} g₀"
+            )
 
 
 # -- Main ---------------------------------------------------------------------
