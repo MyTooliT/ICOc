b8491c8f6fd18eb4e99e4d8c24500b7216b0321e
René Schwaiger <sanssecours@me.com>
2024-01-18 10:04:30 +0100

    Experiments: Measure connection time

--- mytoolit/experiments/new.py
+++ mytoolit/experiments/new.py
@@ -13,28 +13,20 @@ from mytoolit.can import Network
 
 async def test(identifier):
     async with Network() as network:
-        node = "STH 1"
-
+        start = time()
         await network.connect_sensor_device(identifier)
-        name = await network.get_name(node)
-        mac_address = await network.get_mac_address(node)
-        print(
-            f"Connected to sensor device “{name}” with MAC "
-            f"address “{mac_address}”"
-        )
-
-        async with network.open_data_stream(first=True) as stream:
-            async for data in stream:
-                utc_timestamp = time()
-                message_timestamp = data.first.pop().timestamp
-                offset = abs(message_timestamp - utc_timestamp)
-                print(f"UTC timestamp:     {utc_timestamp}")
-                print(f"Message timestamp: {message_timestamp}")
-                print(f"Offset: {offset} ({offset / 60:.3f} minutes)")
-                break
+        print(f"Time until device connection: {time() - start:.3f} seconds")
 
 
 # -- Main ---------------------------------------------------------------------
 
+
+def main():
+    for _ in range(10):
+        start = time()
+        run(test(identifier=0))
+        print(f"Overall runtime: {time() - start:.3f} seconds")
+
+
 if __name__ == "__main__":
-    run(test(identifier=0))
+    main()
