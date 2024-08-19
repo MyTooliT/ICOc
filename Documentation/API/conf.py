c1e99f4b1df9faa5fe9d0943d764b9475813482b
René Schwaiger <sanssecours@me.com>
2024-08-19 12:33:58 +0200

    Sphinx: Remove comments

--- Documentation/API/conf.py
+++ Documentation/API/conf.py
@@ -1,10 +1,4 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "ICOc"
 copyright = "2024, Clemens Burgstaller, René Schwaiger"
@@ -12,7 +6,7 @@ author = "Clemens Burgstaller, René Schwaiger"
 release = "1.11.0"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
 
 extensions = [
     "sphinx.ext.autodoc",
@@ -22,6 +16,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "alabaster"
