e82544b34cc04d75fd5ad117894442052f42deaf
René Schwaiger <sanssecours@me.com>
2025-02-26 15:01:04 +0100

    API: Move data conversion code into new namespace

--- mytoolit/utility/__init__.py
+++ mytoolit/utility/__init__.py
@@ -2,6 +2,5 @@
 
 # -- Exports ------------------------------------------------------------------
 
-from .data import convert_bytes_to_text
 from .open import open_file
 from .naming import convert_base64_mac, convert_mac_base64
