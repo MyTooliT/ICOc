8b430a0273f044ac48829a6564274f66dd2eb9c0
René Schwaiger <sanssecours@me.com>
2025-03-05 13:33:39 +0100

    Sensor Device: Add code to set name

--- icotronic/can/sensor.py
+++ icotronic/can/sensor.py
@@ -109,6 +109,82 @@ class SensorDevice:
             node=self.id, device_number=0xFF
         )
 
+    async def set_name(self, name: str) -> None:
+        """Set the name of a sensor device
+
+        Parameters
+        ----------
+
+        name:
+            The new name for the device
+
+        Examples
+        --------
+
+        >>> from asyncio import run
+        >>> from icotronic.can.connection import Connection
+
+        Change the name of a sensor device
+
+        >>> async def test_naming(name):
+        ...     async with Connection() as stu:
+        ...         # We assume that at least one sensor device is available
+        ...         # and that this device currently does not have the name
+        ...         # specified in the variable `name`.
+        ...         async with stu.connect_sensor_device(0) as sensor_device:
+        ...             before = await sensor_device.get_name()
+        ...             await sensor_device.set_name(name)
+        ...             updated = await sensor_device.get_name()
+        ...             await sensor_device.set_name(before)
+        ...             after = await sensor_device.get_name()
+        ...             return before, updated, after
+        >>> before, updated, after = run(test_naming("Hello"))
+        >>> before != "Hello"
+        True
+        >>> updated
+        'Hello'
+        >>> before == after
+        True
+
+        """
+
+        if not isinstance(name, str):
+            raise TypeError("Name must be str, not type(identifier).__name__")
+
+        bytes_name = list(name.encode("utf-8"))
+        length_name = len(bytes_name)
+        if length_name > 8:
+            raise ValueError(
+                f"Name is too long ({length_name} bytes). "
+                "Please use a name between 0 and 8 bytes."
+            )
+
+        node = self.id
+        # Use 0 bytes at end of names that are shorter than 8 bytes
+        bytes_name.extend([0] * (8 - length_name))
+        description = f"name of “{node}”"
+        self_addressing = 0xFF
+
+        # pylint: disable=protected-access
+
+        await self.spu._request_bluetooth(
+            node=node,
+            subcommand=3,
+            device_number=self_addressing,
+            data=bytes_name[:6],
+            description=f"set first part of {description}",
+        )
+
+        await self.spu._request_bluetooth(
+            node=node,
+            subcommand=4,
+            device_number=self_addressing,
+            data=bytes_name[6:] + [0] * 4,
+            description=f"set second part of {description}",
+        )
+
+        # pylint: enable=protected-access
+
 
 # -- Main ---------------------------------------------------------------------
 
@@ -116,7 +192,7 @@ if __name__ == "__main__":
     from doctest import run_docstring_examples
 
     run_docstring_examples(
-        SensorDevice.get_name,
+        SensorDevice.set_name,
         globals(),
         verbose=True,
     )
